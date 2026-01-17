194979a4a2f27a3a37e4db49e48a0917fd4bb6a3
Brendan Ryan <brendan@bridge.xyz>
2026-01-17 19:55:08 +0000

    makes code slightly more robust

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -31,7 +31,9 @@ pub use config::{
     CustomNetwork, CustomToken, EvmConfig, PaymentMethod, SolanaConfig, WalletConfig,
 };
 pub use currency::{currencies, Currency};
-pub use http::{HttpClient, HttpClientBuilder, HttpMethod, HttpResponse};
+pub use http::{
+    find_header, has_header, parse_headers, HttpClient, HttpClientBuilder, HttpMethod, HttpResponse,
+};
 pub use network::{networks, ChainType, Network, NetworkInfo};
 pub use x402::{PaymentPayload, PaymentRequirementsResponse, SettlementResponse};
 
